e0288c049ba2dcca910c6aa50c690ffe76ff2548
Jorge Trivilin <84451671+jorge-trivilin@users.noreply.github.com>
2024-09-17 09:49:09 -0300

    Update build-and-push-docker.sh

--- train/build-and-push-docker.sh
+++ train/build-and-push-docker.sh
@@ -9,7 +9,7 @@ tag_name=$2
 # Get the login command from ECR and execute it directly
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $account_id.dkr.ecr.$region.amazonaws.com
 
-# Create the ECR repository if it doesn't exist
+# Create ECR repository if it doesn't exist
 aws ecr describe-repositories --repository-names $repo_name --region $region || aws ecr create-repository --repository-name $repo_name --region $region
 
 # Build the docker image
@@ -18,11 +18,11 @@ docker build -t $repo_name -f ./train/Dockerfile ./train
 # Tag the docker image
 docker tag $repo_name:latest $account_id.dkr.ecr.$region.amazonaws.com/$repo_name:$tag_name
 
-# Define a variável ECR_URI 
+# Define ECR_URI variable 
 ECR_URI="$account_id.dkr.ecr.$region.amazonaws.com/$repo_name:$tag_name"
 
 # Push the docker image
 docker push $ECR_URI
 
-# Exporta a ECR_URI como uma variável de ambiente
+# Exports ECR_URI as a environment variable Exporta a ECR_URI como uma variável de ambiente
 echo "TF_VAR_image_uri=$ECR_URI" >> $GITHUB_ENV
